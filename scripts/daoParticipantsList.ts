bbffc858dc73d09b239d361eeeb0e03394694a9f
Doug Kent <github@douglaskent.com>
2018-08-15 15:11:26 -0400

    upgrade arc.js, added daoSchemesList

--- scripts/daoParticipantsList.ts
+++ scripts/daoParticipantsList.ts
@@ -3,28 +3,11 @@ import {
   LoggingService,
   LogLevel,
   DAO,
-  ConfigService,
   Web3,
   Address,
-  DecodedLogEntryEvent
+  Participant
 } from "@daostack/arc.js";
 import { BigNumber } from '../node_modules/bignumber.js';
-
-interface FounderSpec {
-  /**
-   * Founders' address
-   */
-  address: string;
-  /**
-   * string | number token amount to be awarded to each founder, in GEN
-   */
-  tokens: string | number;
-  /**
-   * string | number reputation amount to be awarded to each founder,
-   * in units of the Genesis Reputation system.
-   */
-  reputation: string | number;
-}
 
 /**
  * List participants (as measured by reputation) in the given dao, sorted in descending order of reputation,
@@ -43,84 +26,37 @@ export const run = async (web3: Web3, networkName: string, avatar: Address): Pro
 
   await InitializeArcJs();
 
-  let dao: DAO = await DAO.at(avatar);
+  const participants = await getParticipants(avatar, web3);
 
-  // TODO: let DAO do this, when available
-  const participants = await getParticipants(dao, web3);
-
-  participants.sort((a: Participant, b: Participant): number => {
+  participants.sort((a: ParticipantWithPerc, b: ParticipantWithPerc): number => {
     return (b.reputation.sub(a.reputation)).toNumber();
   });
 
-  console.log(`address : reputation : percent of total `);
+  console.log(`address                                    | reputation\t|\tpercent of total`);
   participants.forEach((p) => {
-    console.log(`${p.address} : ${web3.fromWei(p.reputation).toString(10)} : ${p.percentageOfTotal.toFixed(2)}`);
+    console.log(`${p.address} | ${web3.fromWei(p.reputation).toString(10)}\t|\t${p.percentageOfTotal.toFixed(2)}`);
   });
 
   return Promise.resolve();
 }
 
-export interface Participant {
-  address: Address;
-  reputation?: BigNumber;
+interface ParticipantWithPerc extends Participant {
   percentageOfTotal?: number;
 }
 
-export interface ReputationMintEventResult {
-  /**
-   * The recipient of reputation
-   * indexed
-   */
-  _to: Address;
-  /**
-   * Amount minted
-   */
-  _amount: BigNumber;
-}
+const getParticipants = async (avatar: Address, web3: Web3): Promise<Array<ParticipantWithPerc>> => {
 
-const getParticipants = async (dao: DAO, web3: Web3): Promise<Array<Participant>> => {
+  const dao: DAO = await DAO.at(avatar);
 
-  const addresses = new Set<Address>();
-  let participants: Array<Participant>;
-
-  const fetcher = dao.reputation.Mint({}, Object.assign({ fromBlock: 0 }));
-
-  const eventLog = await new Promise<Array<DecodedLogEntryEvent<ReputationMintEventResult>>>(
-    (resolve: (result: Array<DecodedLogEntryEvent<ReputationMintEventResult>>) => void,
-      reject: (error: Error) => void): void => {
-
-      fetcher.get((error: Error, events: DecodedLogEntryEvent<ReputationMintEventResult> | Array<DecodedLogEntryEvent<ReputationMintEventResult>>): void => {
-        if (error) {
-          return reject(error);
-        }
-        if (!Array.isArray(events)) {
-          events = [events];
-        }
-        return resolve(events);
-      });
-    });
-
-  eventLog.forEach(
-    (event: DecodedLogEntryEvent<ReputationMintEventResult>): void => {
-      addresses.add(event.args._to);
-    });
-
-  participants = new Array<Participant>();
-
-  for (const account of addresses) {
-    const balance = await dao.reputation.reputationOf(account);
-    if (balance.gt(0)) {
-      participants.push({ address: account, reputation: balance });
-    }
-  }
+  let participants = await dao.getParticipants({ returnReputations: true });
 
   const totalReputation = participants
-    .map((p: Participant) => p.reputation)
+    .map((p: ParticipantWithPerc) => p.reputation)
     .reduce((prev: BigNumber, current: BigNumber) => prev.add(current));
 
   console.log(`total reputation: ${web3.fromWei(totalReputation)}`);
 
-  participants.forEach((p: Participant) => { p.percentageOfTotal = p.reputation.div(totalReputation).toNumber() * 100; })
+  participants.forEach((p: ParticipantWithPerc) => { p.percentageOfTotal = p.reputation.div(totalReputation).toNumber() * 100; })
 
   return participants;
 }
